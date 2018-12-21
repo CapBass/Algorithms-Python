e4f8f2b56099ea56b26626a2741f8fdc8dc41d31
Andrew <CapBass@mail.ru>
2018-12-21 10:18:25 +1000

    переделал первое задание и добавил проверку на равенство координат по оси абцисс в третьем

--- task_1.py
+++ task_1.py
@@ -1,11 +1,11 @@
 # Найти сумму и произведение цифр трехзначного числа,
 # которое вводит пользователь.
 
-print('Введите три целых трехзначных числа')
-a = int(input('введите первое целое трехзначное число '))
-b = int(input('введите второе целое трехзначное число '))
-c = int(input('введите третье целое трехзначное число '))
-summ = a + b + c
-product = a * b * c
-print(f'Сумма чисел {a}, {b} и {c} равна {summ}')
-print(f'Произведение чисел {a}, {b} и {c} равно {product}')
+num = int(input('Введите трехзначное число '))
+numeral_1 = int(num / 100)
+numeral_2 = int((num % 100) / 10)
+numeral_3 = (num % 100) % 10
+summ = numeral_1 + numeral_2 + numeral_3
+product = numeral_1 * numeral_2 * numeral_3
+print(f'Сумма цифр {numeral_1}, {numeral_2} и {numeral_3} равна {summ}')
+print(f'Произведение чисел {numeral_1}, {numeral_2} и {numeral_3} равно {product}')
