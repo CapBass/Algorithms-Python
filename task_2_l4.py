8a577c37d9ddd879c802117b7651049d524a996e
Andrew <CapBass@mail.ru>
2019-01-18 09:57:38 +1000

    Исправлено второе задание. Новые выводы

--- task_2_l4.py
+++ task_2_l4.py
@@ -1,12 +1,18 @@
 '''Написать два алгоритма нахождения i-го по счёту простого числа'''
 
 
-# impoert cProfile
+# import cProfile
 # python -m timeit -n 100 -s "import task_3" "task_3.fib(10)"
 
 
-def not_sieve(N):
+def not_sieve(count):    
+    if count == 1:
+        return 2
+    else:        
+        N = count ** 2
+        
     prime_numbers = [2]
+    
     for i in range(3, N + 1, 2): # все нечетные
         if i > 10 and i % 5 == 0: # проверка что число не делится на 5
             continue
@@ -17,25 +23,32 @@ def not_sieve(N):
             if i % num == 0:
                 break
         else:
-            prime_numbers.append(i)    
-    return len(prime_numbers)
+            prime_numbers.append(i)
+        if len(prime_numbers) == count:
+            break
+    return prime_numbers[len(prime_numbers) - 1]
 
 	
 # timeit
-# 100 loops, best of 3: 121 usec per loop - 199
-# 100 loops, best of 3: 14.4 msec per loop - 10249
-# 100 loops, best of 3: 34.1 msec per loop - 20483
-# 100 loops, best of 3: 81 msec per loop - 40849
-# 100 loops, best of 3: 252 msec per loop - 100523
+# 100 loops, best of 3: 147 usec per loop - 50
+# 100 loops, best of 3: 415 usec per loop - 100
+# 100 loops, best of 3: 1.11 msec per loop - 200
+# 100 loops, best of 3: 2.99 msec per loop - 400
+# 100 loops, best of 3: 7.95 msec per loop - 800
 
 # cProfile
 #    ncalls  tottime  percall  cumtime  percall filename:lineno(function)
-#       1    0.018    0.018    0.018    0.018 <ipython-input-130-7f4218183894>:2(not_sieve) - 10249
-#       1    0.054    0.054    0.054    0.054 <ipython-input-130-7f4218183894>:2(not_sieve) - 20483
-#       1    0.101    0.101    0.101    0.101 <ipython-input-130-7f4218183894>:2(not_sieve) - 40849
-#       1    0.275    0.275    0.276    0.276 <ipython-input-130-7f4218183894>:2(not_sieve) - 100523
+#       1    0.011    0.011    0.012    0.012 <ipython-input-32-3eed162a8970>:8(not_sieve) - 1000
+#       1    0.126    0.126    0.129    0.129 <ipython-input-32-3eed162a8970>:8(not_sieve) - 5000
+#       1    0.291    0.291    0.294    0.294 <ipython-input-32-3eed162a8970>:8(not_sieve) - 10000
 
-def sieve(N):
+
+def sieve(count):
+    if count == 1:
+        return 2
+    else:        
+        N = count ** 2
+    
     numeracy = [i for i in range(N + 1)]   
     
     numeracy[1] = 0
@@ -53,26 +66,27 @@ def sieve(N):
     for i in numeracy:
         if numeracy[i] != 0:
             prime_numbers.append(numeracy[i])
+        if len(prime_numbers) == count:
+            break
 
     del numeracy
-    return len(prime_numbers)
+    return prime_numbers[len(prime_numbers) - 1]
 
 
 # timeit
-# 100 loops, best of 3: 58.8 usec per loop - 199
-# 100 loops, best of 3: 3.76 msec per loop - 10249
-# 100 loops, best of 3: 7.78 msec per loop - 20483
-# 100 loops, best of 3: 16.1 msec per loop - 40849
-# 100 loops, best of 3: 43.1 msec per loop - 100523
+# 100 loops, best of 3: 783 usec per loop - 50
+# 100 loops, best of 3: 3.38 msec per loop - 100
+# 100 loops, best of 3: 14.2 msec per loop - 200
+# 100 loops, best of 3: 64.9 msec per loop - 400
+# 100 loops, best of 3: 313 msec per loopp - 800
 
 # cProfile	
 #    ncalls  tottime  percall  cumtime  percall filename:lineno(function)
-#        1    0.006    0.006    0.007    0.007 <ipython-input-125-6e43b0dd8d33>:1(sieve) - 10249
-#        1    0.010    0.010    0.011    0.011 <ipython-input-125-6e43b0dd8d33>:1(sieve) - 20483
-#        1    0.021    0.021    0.023    0.023 <ipython-input-125-6e43b0dd8d33>:1(sieve) - 40849
-#        1    0.067    0.067    0.073    0.073 <ipython-input-125-6e43b0dd8d33>:1(sieve) - 100523
+#        1    0.442    0.442    0.540    0.540 <ipython-input-32-3eed162a8970>:46(sieve) - 1000
+#        1   12.843   12.843   14.480   14.480 <ipython-input-32-3eed162a8970>:46(sieve) - 5000
+#        1   54.403   54.403   61.389   61.389 <ipython-input-32-3eed162a8970>:46(sieve) - 10000
 
 
-'''Вывод: алгоритм поиска делителей и решето Эратосфена имеют одинаковую асимптотическую сложность О(n),
-но алгоритм решето эратосвена показывает на отметках примерно 3х кратную производительность по сравнению 
-с алгоритма поиска делителей, следовательно в данной реализации решето Эратосфена лучше.'''
+
+'''Вывод: алгоритм исключения делителей имеет асимптотическую сдложность О(1.5N), а решето Эратосфена имеет асимптотическую сложность О(N**2),
+следовательно алгоритм поиска i-ого по счету простого числа методом исключения делителей существенно быстрее.'''
